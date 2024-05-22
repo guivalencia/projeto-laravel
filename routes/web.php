e9a46647a231429d6a226ebe753b507c24ba8183
KnX-I <57770213+KnX-I@users.noreply.github.com>
2024-05-22 16:15:37 -0400

    Criação controladores e rotas

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::post('/login', 'LoginController@autenticar')->name('site.login');
 Route::middleware('autenticacao:ldap,padrao')->prefix('/app')->group(function() {
     Route::get('/home', 'HomeController@index')->name('app.home');
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
-    Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
 
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
     Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
@@ -40,6 +39,10 @@ Route::middleware('autenticacao:ldap,padrao')->prefix('/app')->group(function() 
 
     Route::resource('produto','ProdutoController');
     Route::resource('produto-detalhe','ProdutoDetalheController');
+
+    Route::resource('cliente', 'ClienteController');
+    Route::resource('pedido', 'PedidoController');
+    Route::resource('pedido-produto', 'PedidoProdutoController');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
