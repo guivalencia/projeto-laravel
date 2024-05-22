ad4194cbd211cd8fd17364543d02b8d3aeb8d285
KnX-I <57770213+KnX-I@users.noreply.github.com>
2024-05-22 16:38:47 -0400

    Tela de listagem de clientes

---
+++ resources/views/app/cliente/index.blade.php
@@ -0,0 +1,56 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Cliente')
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Listagem de Clientes</p>
+        </div>  
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('cliente.create') }}">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 90%;margin-left: auto; margin-right: auto">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->nome }}</td>
+                                <td><a href="{{ route('cliente.show', ['cliente' => $cliente->id]) }}">Visualizar</a></td>
+                                <td>
+                                    <form id="form_{{$cliente->id}}" method="POST" action="{{ route('cliente.destroy' , ['cliente' => $cliente->id]) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <a href="#" onclick="document.getElementById('form_{{$produto->id}}').submit()">Excluir</a>
+                                    </form>
+                                </td>
+                                <td><a href="{{ route('cliente.edit', ['cliente' => $cliente->id]) }}">Editar</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{ $clientes->appends($request)->links() }}
+
+                {{-- {{ $produtos->count() }} total de registros por pagina
+                {{ $produtos->total() }} total de registros da consulta
+                {{ $produtos->firstItem() }} numero do primeiro registro da pagina
+                {{ $produtos->lastItem() }} numero do ultimo registro da pagina --}}
+
+                Exibindo {{ $clientes->count() }} clientes de {{ $clientes->total() }} (de {{ $clientes->firstItem() }} a {{ $clientes->lastItem() }})
+            </div>
+        </div>     
+    </div>
+@endsection
