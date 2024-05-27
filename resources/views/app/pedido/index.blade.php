e45f05a153173692eb96fe1224c309ad31d3fca3
KnX-I <57770213+KnX-I@users.noreply.github.com>
2024-05-27 13:28:26 -0400

    Cadastro de Pedidos

--- resources/views/app/pedido/index.blade.php
+++ resources/views/app/pedido/index.blade.php
@@ -22,12 +22,14 @@
                             <th>Cliente</th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($pedidos as $pedido)
                             <tr>
-                                <td>{{ $pedido->nome }}</td>
+                                <td>{{ $pedido->id }}</td>
+                                <td>{{ $pedido->cliente_id }}</td>
                                 <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</a></td>
                                 <td>
                                     <form id="form_{{$pedido->id}}" method="POST" action="{{ route('pedido.destroy' , ['pedido' => $pedido->id]) }}">
