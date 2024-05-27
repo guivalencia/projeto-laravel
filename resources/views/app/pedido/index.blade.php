aab00186cf9d5675dd0e4b8713a334d0bb046a13
KnX-I <57770213+KnX-I@users.noreply.github.com>
2024-05-27 16:34:03 -0400

    Tela cadastrar produtos no pedido

--- resources/views/app/pedido/index.blade.php
+++ resources/views/app/pedido/index.blade.php
@@ -23,6 +23,7 @@
                             <th></th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,6 +31,7 @@
                             <tr>
                                 <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente_id }}</td>
+                                <td><a href="{{ route('pedido-produto.create', ['pedido' => $pedido->id]) }}">Adicionar Produtos</a></td>
                                 <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</a></td>
                                 <td>
                                     <form id="form_{{$pedido->id}}" method="POST" action="{{ route('pedido.destroy' , ['pedido' => $pedido->id]) }}">
